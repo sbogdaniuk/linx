0a3a5d43e02e13941b3bf211f6ff8ee1c6c64eb2
sergeibogdaniuk <sergei.bogdaniuk@gmail.com>
2024-02-27 15:17:49 +0200

    layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from "react";
+import cn from "clsx";
 import "./App.css";
 
 function App() {
   return (
     <div className="flex">
-      <div className="flex h-screen flex-col border-e bg-white ">
+      <div className="flex h-screen flex-col border-e bg-slate-600 text-white">
         <div className="px-4 h-16 flex items-center">
           <span className="flex h-10 w-32 items-center justify-center rounded-lg bg-gray-100 text-xl text-gray-600">
             🌤️ <span className="text-sm ml-1">Logo</span>
@@ -13,23 +14,18 @@ function App() {
 
         <div className="px-4 py-6">
           <ul className="space-y-1">
-            <li>
-              <a
-                href="#"
-                className="block rounded-lg bg-gray-100 px-4 py-2 text-sm font-medium text-gray-700"
-              >
-                Dashboard
-              </a>
-            </li>
-
-            <li>
-              <a
-                href="#"
-                className="block rounded-lg px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700"
-              >
-                Users
-              </a>
-            </li>
+            {[{ label: "Dashboard", active: true }, { label: "Users" }].map((d) => (
+              <li key={d.label}>
+                <a
+                  href="#"
+                  className={cn("block rounded-lg px-4 py-2 text-sm font-medium hover:bg-slate-800", {
+                    "bg-slate-800": d.active,
+                  })}
+                >
+                  {d.label}
+                </a>
+              </li>
+            ))}
           </ul>
         </div>
       </div>
@@ -39,7 +35,7 @@ function App() {
             <h1 className="text-lg font-semibold">Dashboard</h1>
           </div>
         </header>
-        <main className="p-4 bg-gray-100 grow">
+        <main className="p-4 grow">
           <div>Custom variable {process.env.REACT_APP_VARIABLE}</div>
         </main>
       </div>
